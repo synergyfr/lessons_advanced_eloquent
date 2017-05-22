9752b06725c1b59446322647d3cfce2ea37fdb46
Henk Rossouw <hrossouw@methys.com>
2017-05-22 12:25:09 +0200

    1 Setup

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+
+    	\App\Post::unguard();
+
+        $this->call(PostsTableSeeder::class);
     }
 }
